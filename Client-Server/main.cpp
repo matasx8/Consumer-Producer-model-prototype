1d6cefe3a579e494475a28ffe4729f1c3de7273b
Matas <m.tunkevicius@gmail.com>
2022-06-22 16:07:56 +0300

    added entt.hpp

--- Client-Server/main.cpp
+++ Client-Server/main.cpp
@@ -7,13 +7,60 @@
 #include <memory>
 #include <array>
 #include "Tests.h"
+#include "extern/ENTT/entt.hpp"
+
+// https://skypjack.github.io/entt/md_docs_md_entity.html
+// do a fake example where we update positions of entities and etc on the front-end
+// and render them on the back end
+
+
+
+struct position {
+    float x;
+    float y;
+};
+
+struct velocity {
+    float dx;
+    float dy;
+};
+
+void update(entt::registry& registry) {
+    auto view = registry.view<const position, velocity>();
+
+    // use a callback
+    view.each([](const auto& pos, auto& vel) { /* ... */ });
+
+    // use an extended callback
+    view.each([](const auto entity, const auto& pos, auto& vel) { /* ... */ });
+
+    // use a range-for
+    for (auto [entity, pos, vel] : view.each()) {
+        // ...
+    }
+
+    // use forward iterators and get only the components of interest
+    for (auto entity : view) {
+        auto& vel = view.get<velocity>(entity);
+        // ...
+    }
+}
 
 
 int main()
 {
-	tests::ProducerConsumerThreaded_AllWorkToConsumer();
-	tests::ProducerConsumerThreaded_SharedWork();
-	tests::ProducerConsumerNonThreaded_SharedWork();
+    entt::registry registry;
+
+    for (auto i = 0u; i < 10u; ++i) {
+        const auto entity = registry.create();
+        registry.emplace<position>(entity, i * 1.f, i * 1.f);
+        if (i % 2 == 0) { registry.emplace<velocity>(entity, i * .1f, i * .1f); }
+    }
+
+    update(registry);
+	//tests::ProducerConsumerThreaded_AllWorkToConsumer();
+	//tests::ProducerConsumerThreaded_SharedWork();
+	//tests::ProducerConsumerNonThreaded_SharedWork();
 	return 0;
 }
 
